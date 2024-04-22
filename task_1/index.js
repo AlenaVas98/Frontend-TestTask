23884a81d36baa813a16870323e8b11f65653909
Алёна Ващан <alena_iv1998@mail.ru>
2024-04-22 12:20:21 +0300

    task 1

--- task_1/index.js
+++ task_1/index.js
@@ -1,8 +1,36 @@
-import {encoded, translations} from './data.js'
+import { encoded, translations } from "./data.js";
 
-console.log("Let's rock")
-console.log(encoded, translations)
+export function decodeFields(encoded, translations) {
+  // Создаем копию исходного списка объектов
+  const decoded = JSON.parse(JSON.stringify(encoded));
 
+  // Проходимся по каждому объекту
+  decoded.forEach((obj) => {
+    // Проходимся по каждому ключу в объекте
+    for (const key in obj) {
+      // Если ключ заканчивается на "Id" и не является одним из исключенных полей
+      if (
+        key.endsWith("Id") &&
+        !["groupId", "service", "formatSize", "ca"].includes(key)
+      ) {
+        const value = obj[key];
+        // Если значение ключа есть в словаре translations, заменяем его
+        if (translations[value]) {
+          obj[key] = translations[value];
+        }
+      }
+    }
+  });
 
+  // Получаем список уникальных id
+  const uniqueIds = [...new Set(decoded.map((obj) => obj.groupId))];
 
-// console.log(decoded)
+  return { decoded, uniqueIds };
+}
+
+// Пример использования функции
+const { decoded, uniqueIds } = decodeFields(encoded, translations);
+console.log("Расшифрованные данные:");
+console.log(decoded);
+console.log("Уникальные id:");
+console.log(uniqueIds);
